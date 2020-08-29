16a89c160edefc5b1e9f3c3257d1529fb2a8f6cb
Billy <billyjcatchpole@gmail.com>
2020-08-29 12:57:16 +0100

    A

--- front-end/src/Util/send_axios_request.ts
+++ front-end/src/Util/send_axios_request.ts
@@ -1,6 +1,8 @@
 import axios from "axios"
 
-export const production = "http://localhost:8000/"//dev
+//export const production = "http://localhost:8000/"//dev
+export const production = "https://birdie-test-billy.herokuapp.com/"//production
+
 
 const send_axios_request = (url:string, body:object, type?:string) => {
 
